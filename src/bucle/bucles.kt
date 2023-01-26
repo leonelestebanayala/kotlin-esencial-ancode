98729fe0bc4a445ad301edeaa21b3dcf24298251
leonelestebanayala <leonelestebanayala@gmail.com>
2023-01-26 01:15:08 -0300

    bubles

---
+++ src/bucle/bucles.kt
@@ -0,0 +1,10 @@
+package bucle
+
+fun main() {
+    var potenciaDeDos = 2
+    while (potenciaDeDos <= 3000) {
+        println(potenciaDeDos)
+        potenciaDeDos = potenciaDeDos * 2
+    }
+    println(potenciaDeDos)
+}
---
+++ src/bucle/tabla.kt
@@ -0,0 +1,14 @@
+package bucle
+
+fun main() {
+
+    val y = 4
+    var z = 0
+
+    for (x in 0..10) {
+        z = y * x;
+        println("$y * $x = $z")
+
+    }
+
+}
